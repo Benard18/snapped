1eb5882b9a784a46fd134158d905b83a0952f30b
Neville <kilonzoneville@gmail.com>
2018-05-17 22:43:34 +0300

    add working business logic

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,19 +1,7 @@
 
 
-function myFunction() {
-  var dis = Math.floor((Math.random() * 2) + 1)
-    if (dis === 1) {
-      alert("You Die")
-    }
-     else {
-alert("You Live")
-    }
 
-
-}
- 
-
-
+dis = Math.floor((Math.random() * 2) + 1)
 
 
 
@@ -21,41 +9,40 @@ alert("You Live")
 
 $(document).ready(function() {
   $("button").click(function() {
-    myFunction();
+    $("#dir").attr("href","index"+dis+".html");
   });
 });
 
-///////User INterface///////
-$(document).ready(function (){
-  $(function(){
-
-    /*
-    Add this code to every page.
-
-    We start by hiding the body, and then fading it in.
-    */
-    $(".marvel-title").hide().fadeIn('.container-fluid');
-
-    /*
-    Now we deal with all 'a' tags...
-    */
-    $('a').click(function(){
-        /*
-        Get the url from this anchors href
-        */
-        var link = $(this).attr('href');
-        /*
-        Fade out the whole page
-        */
-        $(".marvel-title").fadeOut(".container-fluid", function(){
-            /*
-            When that's done (on the 'callback') send the browser to the link.
-            */
-            window.location.href = link;
-            $("#death").show()
-        });return false;
-    });
-$("#death").hide()
-});
-
-});
+// ///////User INterface///////
+// $(document).ready(function (){
+//   $(function(){
+//
+//     /*
+//     Add this code to every page.
+//
+//     We start by hiding the body, and then fading it in.
+//     */
+//     $("body").hide().fadeIn('index1.html');
+//     /*
+//     Now we deal with all 'a' tags...
+//     */
+//     $('a').click(function(){
+//       myFunction();
+//         /*
+//         Get the url from this anchors href
+//         */
+//         var link = $(this).attr('href',"index"+dis+".html");
+//         /*
+//         Fade out the whole page
+//         */
+//         $("index4.html").fadeOut("index1.html", function(){
+//             /*
+//             When that's done (on the 'callback') send the browser to the link.
+//             */
+//             window.location.href = link;
+//             $("link").show()
+//         });return false;
+//     });
+// });
+//
+// });
